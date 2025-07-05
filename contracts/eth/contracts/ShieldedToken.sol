4349ee15df1ead534713846c11b2dc3d4661237f
AlehNat <natsevsky@gmail.com>
2025-07-05 13:44:11 +0200

    add transferFrom

--- contracts/eth/contracts/ShieldedToken.sol
+++ contracts/eth/contracts/ShieldedToken.sol
@@ -26,6 +26,9 @@ contract ShieldedToken {
     /// @notice Mapping of encrypted token balances for each address
     /// @dev The balance is stored as a handle that requires encryption and decryption under the user's secret key to get the actual value
     mapping(address => gtUint64) private balances;
+    /// @notice Mapping of encrypted allowances for each address pair
+    /// @dev The allowance is stored as a handle that requires decryption to get the actual value
+    mapping(address => mapping(address => gtUint64)) private allowances;
     address public owner;
     IERC20 public underlying;
 
@@ -88,6 +91,47 @@ contract ShieldedToken {
         return result;
     }
 
+    /// @notice Transfers the amount of tokens given inside the IT (an encrypted and signed value) from one address to another
+    /// @param _from The address to transfer from
+    /// @param _to The address to transfer to
+    /// @param _it The encrypted and signed transfer amount
+    /// @return The handle to the transfer's result
+    function transferFrom(address _from, address _to, itUint64 calldata _it) public returns (gtBool) {
+        gtUint64 value = MpcCore.validateCiphertext(_it);
+        MpcCore.permitTransient(value, msg.sender); // Give transient permission the the sender, so the contractTransferFrom check will pass
+        return contractTransferFrom(_from, _to, value);
+    }
+    /// @notice Transfers the amount of tokens given in the clear from one address to another
+    /// @param _from The address to transfer from
+    /// @param _to The address to transfer to
+    /// @param _value The amount of tokens to transfer
+    /// @return The handle to the transfer's result
+    function transferFrom(address _from, address _to, uint64 _value) public returns (gtBool) {
+        (gtUint64 fromBalance, gtUint64 toBalance) = _getBalances(_from, _to);
+        gtUint64 allowance = _getGTAllowance(_from, msg.sender);
+        (gtUint64 newFromBalance, gtUint64 newToBalance, gtBool result, gtUint64 newAllowance) = MpcCore.transferWithAllowance(fromBalance, toBalance, _value, allowance);
+        _setApproveValue(_from, msg.sender, newAllowance);
+        _setNewBalances(_from, _to, newFromBalance, newToBalance);
+        emit Transfer(_from, _to, _value);
+        return result;
+    }
+    /// @notice Transfers the amount of tokens given in the handle from one address to another
+    /// @param _from The address to transfer from
+    /// @param _to The address to transfer to
+    /// @param _value The encrypted transfer amount
+    /// @return The handle to the transfer's result
+    function contractTransferFrom(address _from, address _to, gtUint64 _value) public returns (gtBool) {
+        // Check if the sender is permitted to use the _value handle
+        require(MpcCore.isSenderPermitted(_value));
+        (gtUint64 fromBalance, gtUint64 toBalance) = _getBalances(_from, _to);
+        gtUint64 allowance = _getGTAllowance(_from, msg.sender);
+        (gtUint64 newFromBalance, gtUint64 newToBalance, gtBool result, gtUint64 newAllowance) = MpcCore.transferWithAllowance(fromBalance, toBalance, _value, allowance);
+        _setApproveValue(_from, msg.sender, newAllowance);
+        _setNewBalances(_from, _to, newFromBalance, newToBalance);
+        emit Transfer(_from, _to);
+        return result;
+    }
+
     function _getBalances(address _from, address _to) private view returns (gtUint64, gtUint64) {
         return (_balanceOf(_from), _balanceOf(_to));
     }
@@ -115,6 +159,29 @@ contract ShieldedToken {
         // Permit the contract and the _to to use the balance handle
         MpcCore.permitThis(newToBalance);
         MpcCore.permit(newToBalance, _to);
+    }
+
+    /// @notice Returns the handle to the allowance of a spender
+    /// @param _owner The address of the token owner
+    /// @param _spender The address of the spender
+    /// @return The allowance handle
+    function _getGTAllowance(address _owner, address _spender) private view returns (gtUint64) {
+        if (gtUint64.unwrap(allowances[_owner][_spender]) == 0) {
+            return zero;
+        } else {
+            return allowances[_owner][_spender];
+        }
+    }
+
+    /// @notice Sets the new handle to the allowance for a spender
+    /// @param _owner The address of the token owner
+    /// @param _spender The address of the spender
+    /// @param _value The new allowance handle
+    function _setApproveValue(address _owner, address _spender, gtUint64 _value) private {
+        allowances[_owner][_spender] = _value;
+        // Permit the contract and the _owner user to use the allowance handle
+        MpcCore.permitThis(_value);
+        MpcCore.permit(_value, _owner);
     }
     
     /**
