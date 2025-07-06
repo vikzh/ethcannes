3db29266ef7e06a86662f2ccc305d3427e3693f7
IlyaTsupryk <ilya.tsupryk@gmail.com>
2025-07-06 02:23:42 +0200

    Add configs for hardhat SCs verification

--- contracts/eth/hardhat.config.ts
+++ contracts/eth/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import "@nomicfoundation/hardhat-verify";
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -28,6 +29,12 @@ const config: HardhatUserConfig = {
       gasMultiplier: 1.2,
     },
   },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true,
+  }
 };
 
 export default config;
